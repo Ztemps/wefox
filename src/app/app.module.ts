9defe96c3534bc59488087a33624dc7ee0e3c870
Javi <javicasados@gmail.com>
2018-08-08 23:14:05 +0200

    Home Created
    - Removed default angular content
    - Routes has been created
    - Added router-outlet
    - Home Works message it's now visible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from '../pages/home/home.component';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { appRoutes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, { useHash: true}),
     BrowserModule
   ],
   providers: [],
