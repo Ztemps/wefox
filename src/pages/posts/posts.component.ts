4d38797e37dcbb47a3a0f2a1e4914335eb3c7574
Javi <javicasados@gmail.com>
2018-08-09 14:12:20 +0200

    Added click functions
    - Now it's possible to go home if you click on the header logo.
    - Added a new button "Create post"

--- src/pages/posts/posts.component.ts
+++ src/pages/posts/posts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from './posts.service';
 import { Post } from '../../models/post.model';
+import { Router } from '../../../node_modules/@angular/router';
+import { BsModalRef, BsModalService } from '../../../node_modules/ngx-bootstrap';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-posts',
@@ -8,7 +11,12 @@ import { Post } from '../../models/post.model';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
-  constructor(private _postsService: PostsService) { }
+  constructor(
+    private _postsService: PostsService,
+    private router: Router,
+    public bsModalRef: BsModalRef,
+    private _modalService: BsModalService,
+  ) { }
 
   public posts: [{}] = [{}];
   public isLoaded = false;
@@ -27,4 +35,15 @@ export class PostsComponent implements OnInit {
       err => console.error('Error gathering the post list', err)
     );
   }
+
+  public goHome() {
+    this.router.navigate(['/home']);
+  }
+
+  createPost() {
+    const initialState = {};
+    this.bsModalRef = this._modalService.show(ModalComponent, {initialState});
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
+
 }
