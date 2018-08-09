b59664d3e85a06a6de2c8175519943c21bf18873
Javi <javicasados@gmail.com>
2018-08-09 17:22:58 +0200

    Get single post
    - Created structure (no design) just console.logs
    - Call method, service, and module configuration

--- src/pages/posts/post/post.component.ts
+++ src/pages/posts/post/post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
+import { PostService } from './post.service';
+import { Post } from '../../../models/post.model';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  public selectedPost: number;
+  public isLoaded = false;
+
+  constructor(
+    private actRoute: ActivatedRoute,
+    private _postService: PostService) { }
 
   ngOnInit() {
+    console.log('this.actRoute.params: ', this.actRoute.params['value'].id);
+    this.selectedPost =  this.actRoute.params['value'].id;
+    this._postService.getSinglePost(this.selectedPost).subscribe((res: Post) => {
+      console.log('res: ', res);
+    });
   }
 
 }
--- src/pages/posts/post/post.module.ts
+++ src/pages/posts/post/post.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PostComponent } from './post.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { PostService } from './post.service';
 
 @NgModule({
     imports: [SharedModule],
     declarations: [PostComponent],
     exports: [],
-    providers: []
+    providers: [PostService]
 })
 export class PostModule { }
