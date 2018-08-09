463b2065fa3652db9c9660bfa61188ca0ccd5d81
Javi <javicasados@gmail.com>
2018-08-09 17:34:15 +0200

    Started single post page design
    - Just added as usual the header and loader (It's a good idea do it global and no repeat code)

--- src/pages/posts/post/post.component.ts
+++ src/pages/posts/post/post.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '../../../../node_modules/@angular/router';
+import { ActivatedRoute, Router } from '../../../../node_modules/@angular/router';
 import { PostService } from './post.service';
 import { Post } from '../../../models/post.model';
 
@@ -15,14 +15,20 @@ export class PostComponent implements OnInit {
 
   constructor(
     private actRoute: ActivatedRoute,
-    private _postService: PostService) { }
+    private _postService: PostService,
+    private router: Router) { }
 
   ngOnInit() {
     console.log('this.actRoute.params: ', this.actRoute.params['value'].id);
     this.selectedPost =  this.actRoute.params['value'].id;
     this._postService.getSinglePost(this.selectedPost).subscribe((res: Post) => {
       console.log('res: ', res);
+      this.isLoaded = true;
     });
+  }
+
+  public goHome() {
+    this.router.navigate(['/home']);
   }
 
 }
